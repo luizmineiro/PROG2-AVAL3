627f63315e8c2807c3836a065ec88a535aaa5002
0_R3tr0 <79876781+FSFAraujo@users.noreply.github.com>
2022-08-14 16:35:14 -0300

    elements stored

--- lib/elements.dart
+++ lib/elements.dart
@@ -5,6 +5,7 @@ class Elements{
  late File raw;
  late String elem;
  late List <String> table;
+ List <Element> list = [];
  late Element current;
  
  Elements(){
@@ -12,10 +13,16 @@ class Elements{
   elem = raw.readAsStringSync();
   table = elem.split('\n'); 
   table.removeAt(0);
-  for(int x = 0; x <= table.length; x++){
-    String line = table[0];
+  table.removeAt(table.length-1);
+  for(int x = 0; x < table.length; x++){
+    String line = table[x];
     List <dynamic> info = line.split(',');
     current = Element(info[0], info[1], info[2], info[3]);
+    list.add(current);
   }
+ }
+ 
+ List forEach(a){
+  return list;
  }
 }
