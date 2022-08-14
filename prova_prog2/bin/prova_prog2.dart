f3e03db041f582152e236d224cf16bde6932fd81
João Luiz <jluizmineiro@gmail.com>
2022-08-14 10:43:47 -0300

    update

---
+++ prova_prog2/bin/prova_prog2.dart
@@ -0,0 +1,5 @@
+import 'package:prova_prog2/prova_prog2.dart' as prova_prog2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${prova_prog2.calculate()}!');
+}
---
+++ prova_prog2/test/prova_prog2_test.dart
@@ -0,0 +1,8 @@
+import 'package:prova_prog2/prova_prog2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
